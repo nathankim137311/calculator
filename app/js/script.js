e72ba93e0b1db13c33f31202feb3dca9e3f468ec
web-dev <nathankim137311@gmail.com>
2021-09-23 16:38:17 -0700

    completed basic styling of the calculator

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,30 @@
 console.log('hello world!');
+
+function add (a, b) {
+return a + b; 
+}
+
+function subtract (a, b) {
+return a - b; 
+}
+
+function multiply (a, b) {
+return a * b; 
+}
+
+function divide (a, b) {
+return a / b; 
+}
+
+console.log(add(1, 2)); 
+console.log(subtract(1, 2)); 
+console.log(multiply(1, 2)); 
+console.log(divide(1, 2)); 
+
+function operate (operator, num1, num2) {
+  if (operator == '+') {
+    return add(num1, num2); 
+  }
+}
+
+console.log(operate('+', 1, 2)); 
