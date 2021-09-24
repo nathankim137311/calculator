61acd554212c8e7afd54b6be66635bf39975a1d0
web-dev <nathankim137311@gmail.com>
2021-09-23 22:07:28 -0700

    calculator functions work

--- app/js/script.js
+++ app/js/script.js
@@ -9,6 +9,7 @@ const display = document.getElementById('display');
 let num1 = 0; 
 let num2 = 0; 
 let operator = 0; 
+let timesClicked = 0; 
 
 // event listeners 
 // display converts to empty string 
@@ -19,55 +20,70 @@ clearBtn.addEventListener('click', () => {
 // all number buttons display text content 
 numberBtns.forEach((currentBtn) => {
   currentBtn.addEventListener('click', (e) => {
-    display.textContent += e.target.textContent; 
-    num1 = display.textContent; 
+    if (timesClicked === 0) {
+      display.textContent += e.target.textContent; 
+    } else {
+      display.textContent = e.target.textContent; 
+    }
   });
 });
 
 // operators 
 operatorBtns.forEach((currentBtn) => {
   currentBtn.addEventListener('click', (e) => {
+    num1 = display.textContent; 
+    display.textContent = '';
       if (e.target.textContent === '+') {
-        console.log('addition');
+        operator = '+';  
+        operate(operator, num1);
       } else if (e.target.textContent === '-') {
-        console.log('subtraction');
-      } else if (e.target.textContent === 'X') {
-        console.log('multiplication');
+        operator = '-'; 
+        operate(operator, num1); 
+      } else if (e.target.textContent === 'x') {
+        operator = 'x'; 
+        operate(operator, num1); 
       } else if (e.target.textContent === '/') {
-        console.log('division'); 
+        operator = '/'; 
+        operate(operator, num1); 
       }
   });
 });
 
 // equals button 
 equalsBtn.addEventListener('click', () => {
-  operate(operator, num1, num2);
+  num2 = display.textContent; 
+  console.log(num2); 
+  console.log(operator);
+  console.log(num1); 
+  display.textContent = operate(operator, num1, num2);
 });
 
 
 // calculator functions 
 function add (num1, num2) {
-return num1 + num2; 
+return Number(num1) + Number(num2); 
 }
 
 function subtract (num1, num2) {
-return num1 - num2; 
+return Number(num1) - Number(num2); ; 
 }
 
 function multiply (num1, num2) {
-return num1 * num2; 
+return Number(num1) * Number(num2); ; 
 }
 
 function divide (num1, num2) {
-return num1 / num2; 
+return Number(num1) / Number(num2); ; 
 }
-
-console.log(add(1, 2));  
 
 function operate (operator, num1, num2) {
   if (operator == '+') {
     return add(num1, num2); 
+  } else if (operator == '-') {
+    return subtract(num1, num2); 
+  } else if (operator == 'x') {
+    return multiply(num1, num2); 
+  } else if (operator == '/') {
+    return divide(num1, num2); 
   }
 }
-
-console.log(operate('+', 1, 2)); 
