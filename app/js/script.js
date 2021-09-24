f2eaa59acee40cf1b7b0eb10746e9935faa59524
web-dev <nathankim137311@gmail.com>
2021-09-23 18:18:58 -0700

    added event listeners to all buttons. Number inputs can be displayed.

--- app/js/script.js
+++ app/js/script.js
@@ -1,25 +1,68 @@
 console.log('hello world!');
+// variables 
+const clearBtn = document.getElementById('clear'); 
+const numberBtns = Array.from(document.querySelectorAll('.numbers')); 
+const operatorBtns = Array.from(document.querySelectorAll('.operators'));
+const equalsBtn = document.getElementById('equals');
+const display = document.getElementById('display');
 
-function add (a, b) {
-return a + b; 
+let num1 = 0; 
+let num2 = 0; 
+let operator = 0; 
+
+// event listeners 
+// display converts to empty string 
+clearBtn.addEventListener('click', () => {
+  display.textContent = ''; 
+});
+
+// all number buttons display text content 
+numberBtns.forEach((currentBtn) => {
+  currentBtn.addEventListener('click', (e) => {
+    display.textContent += e.target.textContent; 
+    num1 = display.textContent; 
+  });
+});
+
+// operators 
+operatorBtns.forEach((currentBtn) => {
+  currentBtn.addEventListener('click', (e) => {
+      if (e.target.textContent === '+') {
+        console.log('addition');
+      } else if (e.target.textContent === '-') {
+        console.log('subtraction');
+      } else if (e.target.textContent === 'X') {
+        console.log('multiplication');
+      } else if (e.target.textContent === '/') {
+        console.log('division'); 
+      }
+  });
+});
+
+// equals button 
+equalsBtn.addEventListener('click', () => {
+  operate(operator, num1, num2);
+});
+
+
+// calculator functions 
+function add (num1, num2) {
+return num1 + num2; 
 }
 
-function subtract (a, b) {
-return a - b; 
+function subtract (num1, num2) {
+return num1 - num2; 
 }
 
-function multiply (a, b) {
-return a * b; 
+function multiply (num1, num2) {
+return num1 * num2; 
 }
 
-function divide (a, b) {
-return a / b; 
+function divide (num1, num2) {
+return num1 / num2; 
 }
 
-console.log(add(1, 2)); 
-console.log(subtract(1, 2)); 
-console.log(multiply(1, 2)); 
-console.log(divide(1, 2)); 
+console.log(add(1, 2));  
 
 function operate (operator, num1, num2) {
   if (operator == '+') {
